2d9a8458f9299aabbf5a9ad84342dac96cdfa47d
Rin Kates <erinkates11@gmail.com>
2023-09-13 21:06:26 -0400

    assets and modules imported

---
+++ game.py
@@ -0,0 +1,16 @@
+import os # os module provides a way to interact with the operating system (such as working with files and directories)
+
+import random # allows you to generate random numbers or perform random operations in your code
+
+import math # provides various mathematical functions and constants for mathematical operations
+
+import pygame # library for creating 2D games in Python (functionality for handling graphics, sound, & user input in games)
+
+#  import the listdir function from the os module
+from os import listdir # (used to list all the files and directories in a specified directory)
+
+# import the isfile and join functions from the os.path module
+from os.path import isfile, join # isfile is used to check if a given path points to a regular file, & join is used to join parts of file paths into a single path
+
+# intitialize pygame module
+pygame.init()
